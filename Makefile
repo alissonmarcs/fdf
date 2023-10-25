deb53acbb88326422f5f00d44dc179e35b286c02
Alisson Marcos Serafim De Souza <almarcos@c1r9p1.42sp.org.br>
2023-10-25 18:02:21 -0300

    other updates

--- Makefile
+++ Makefile
@@ -6,14 +6,17 @@
 #    By: almarcos <almarcos@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/10/17 14:54:44 by almarcos          #+#    #+#              #
-#    Updated: 2023/10/17 15:36:21 by almarcos         ###   ########.fr        #
+#    Updated: 2023/10/25 11:59:54 by almarcos         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 LIBFT = libs/Libft/libft.a
 MLX42 = libs/MLX42/build/libmlx42.a
 MLX42_FLAGS = -ldl -lglfw -pthread -lm
+CFLAGS = -Wall -Werror -Wextra -g -O0
 HEADERS = -I libs/Libft/ -I libs/MLX42/include/
+
+SOURCES = draw_line/draw_line.c
 
 all: run
 
@@ -23,8 +26,13 @@ $(LIBFT):
 	make -C libs/Libft/
 
 $(MLX42):
-	make -C libs/MLX42/build
+	cd libs/MLX42/ && cmake -B build -DDEBUG=1
+	cd libs/MLX42/ && cmake --build build -j4
+
+clean:
+	make -C libs/Libft/ fclean
+	rm -rf libs/MLX42/build
 
 run: libs
-	cc main.c $(MLX42) $(MLX42_FLAGS) $(LIBFT) $(HEADERS)
+	@cc main.c $(SOURCES) $(LIBFT) $(MLX42) $(MLX42_FLAGS) $(HEADERS) -g -O0
 	./a.out
--- main.c
+++ main.c
@@ -1,7 +1,63 @@
-#include <libft.h>
-#include <MLX42/MLX42.h>
+#include "fdf.h"
+#include <time.h> 
+
+void error()
+{
+	ft_putstr_fd((char *)mlx_strerror(mlx_errno), 1);
+	exit(EXIT_FAILURE);
+}
+
+void hook(void *param)
+{
+	t_data *data;
+	data = param;
+
+	if (mlx_is_key_down(data->mlx, MLX_KEY_ESCAPE))
+		mlx_close_window(data->mlx);
+}
 
 int main()
 {
-	ft_printf("255 to hexa: %x\n", 255);
+	t_data data;
+	t_pixel start;
+	t_pixel end;
+	int i;
+
+	//mlx_set_setting(MLX_HEADLESS, 1);
+	data.mlx = mlx_init(WIDTH, HEIGHT, "lines", true);
+	if (!data.mlx)
+		error();
+	data.img = mlx_new_image(data.mlx, WIDTH, HEIGHT);
+	if (!data.img)
+		error();
+	ft_memset(data.img->pixels, 255, data.img->width * data.img->height * sizeof (int32_t));
+
+	mlx_image_to_window(data.mlx, data.img, 0, 0);
+	
+
+	mlx_loop_hook(data.mlx, hook, &data);
+
+	// start.x = 1;
+	// start.y = 1;
+	// start.color = 700;
+	// end.x = 6;
+	// end.y = 2;
+
+	// draw_line(&data, start, end);
+	srand(time(0)); 
+	i = 0;
+	while (i < 900)
+	{
+		start.x = (rand() % ((WIDTH - 1)- 1 + 1)) + 1;
+		start.y = (rand() % ((HEIGHT - 1) - 1 + 1)) + 1;
+		end.x = (rand() % ((WIDTH - 1)- 1 + 1)) + 1;
+		end.y = (rand() % ((HEIGHT - 1) - 1 + 1)) + 1;
+		draw_line(&data, start, end);
+		i++;
+	}
+
+	mlx_loop(data.mlx);
+	mlx_terminate(data.mlx);
+
+	return (0);
 }
