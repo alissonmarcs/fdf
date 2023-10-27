1852fde5af24f85ec4f56e66657dc3dc1c3b130d
Alisson <alissonmarcos249@gmail.com>
2023-10-26 23:09:03 -0300

    adding ft_hex_to_int function

---
+++ libs/Libft/ft_hex_to_int.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_hex_to_int.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alisson <alisson@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/26 16:54:31 by alisson           #+#    #+#             */
+/*   Updated: 2023/10/26 16:55:10 by alisson          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_hex_to_int(char *str)
+{
+	int result;
+	int byte;
+
+	result = 0;
+	while (*str != ' ' && *str)
+	{
+		byte = *str++;
+		if (byte >= '0' && byte <= '9')
+			byte -= '0';
+		else if (byte >= 'a' && byte <= 'f')
+			byte = byte - 'a' + 10;
+		else if (byte >= 'A' && byte <= 'F')
+			byte = byte - 'A' + 10;
+		result = (result << 4) | (byte & 0xF);
+	}
+	return (result);
+}
